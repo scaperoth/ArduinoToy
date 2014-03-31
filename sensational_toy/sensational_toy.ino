f8472a51eed36fdc1c33bcf97218ad174ce9b759
Matthew Scaperoth <mscapero@gwu.edu>
2014-03-31 03:17:39 -0400

    Added proper documentation

--- sensational_toy/sensational_toy.ino
+++ sensational_toy/sensational_toy.ino
@@ -1,3 +1,10 @@
+/**********************************************************
+ * @authors: Matt Scaperoth, Niyi Odumosu, Joseph Burns
+ * @description: various sensors attached to Arduino Leonardo
+ *               with EEPROM recording humidity data that can
+ *               be dumped to a Java program. 
+ **********************************************************
+
 /***************************
  * INCLUDED FILES
  ***************************/
@@ -10,6 +17,8 @@
 
 /***************************
  * SPARKJET VOICEBOX SETUP
+ * GND on SPK-
+ * VCC on SPK+
  ***************************/
 //Define the Pin Numbers for the sketch.
 #define E0  5
@@ -57,8 +66,8 @@ char c02_sounds[] = {
 #define DHTPIN 6     // what pin we're connected to
 
 //RANGER
-#define trigPin 9
-#define echoPin 10
+#define trigPin 9 //trig to pin 9
+#define echoPin 10 //echo to pin 10
 
 /***************************
  * MEM CONTROL VARIABLES
@@ -70,26 +79,43 @@ int address_val = 0;
  * TIMING VARIABLES
  ***************************/
 int timer = 0;
+//delay for whole program
 int global_delay = 100;
-double rom_minutes = .1;
-double humid_speaker_seconds = 4; 
-int rom_delay = rom_minutes * (60000/global_delay);
-int humid_speaker_delay = humid_speaker_seconds * (1000/global_delay);
+
+//delay for various sensors and components
+double rom_minutes = .1; //how often to save sensor data
+double humid_speaker_seconds = 4; //how long to wait to alarm
+
+int rom_delay = rom_minutes * (60000/global_delay); //minutes relative to global delay
+int humid_speaker_delay = humid_speaker_seconds * (1000/global_delay); //seconds relative to global delay
 
 /***************************
  * BARGRAPH SETUP
+ * LAT & SIN to MOSI 
+ *   - LAT behind SIN such that
+ *     SIN receives current first
+ * CLK to SCK
+ * VCC/+5V to 5V
+ * GND to GND
  ***************************/
 SFEbarGraph BG;
 int num_leds = 0;
 
 /***************************
  * HUMIDITY SETUP
+ * DTA to Pin 6
  ***************************/
-
 dht22 DHT22;
 int humidity_val= 0;
 int chk;
+//at what humidity level should the alarm go off
+int humidity_alarm_value = 40; 
 
+/***************************
+ * RANGE FINDER VARIABLES
+ ***************************/
+//at what max range should the alarm go off (in cm)
+int range_alarm_value = 5;
 
 /***************************
  * BEGIN PROGRAM
@@ -128,24 +154,32 @@ void loop(){
  * BARGRAPH FUNCTION(S)
  ***************************/
 /**
- *
+ * get humidity value and convert it to
+ * a number between 0 and 30. Send this
+ * number to fill_leds to light up
+ * the appropriate number of LEDS on the
+ * bargraph breakout kit
  */
 void activate_bargraph(){
   if(check_humidity_sensor()){
-    humidity_val = DHT22.humidity;
-    num_leds = (int)(humidity_val/3.33);
-
-    fill_leds(num_leds);
-
-
+    humidity_val = DHT22.humidity;  //get humidity
+    num_leds = (int)(humidity_val/3.33); //convert to 0-30
+    fill_leds(num_leds); //light up leds
   }
-  if(humidity_val>=40 && timer%humid_speaker_delay ==0){
+  //alarm if humidity is below alarm value and proper increment is reached
+  if(humidity_val>=humidity_alarm_value && timer%humid_speaker_delay ==0){
     //Serial.println("Play Sound");
     speakjet.print(humidity_sounds);
   }
 }
 /**
+ * find the inverse of the max led
+ * so the green leds are the first to light up
+ * then loop through and light each LED from the 
+ * end to the inverse value. 
  *
+ * Example: if max_led is 7 then you will light up
+ *          led #'s 29 - 22;
  */
 void fill_leds(int max_led){
   int inverse = 29-max_led;
@@ -160,7 +194,8 @@ void fill_leds(int max_led){
  * HUMIDITY FUNCTION(S)
  ***************************/
 /**
- *
+ * checks if humidity sensor is ready
+ * reuturns a boolean true or false
  */
 int check_humidity_sensor(){
   chk = DHT22.read();
@@ -180,24 +215,31 @@ int check_humidity_sensor(){
  * RANGER FUNCTION(S)
  ***************************/
 /**
- *
+ * sets all necessary pins for range sensor
+ * used in Setup()
  */
 void setup_ranger(){
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT_PULLUP);
 }
 /**
- *
+ * Scan the range finder values and
+ * alert if the values are below a certain level
  */
 void find_range(){
   int duration, distance;
+  //pulse range finder
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(2000);
   digitalWrite(trigPin, LOW);
   duration = pulseIn(echoPin, HIGH);
+  
+  //convert pulse value to cm
   distance = (duration/2) / 29.1;
+  
   //Serial.println(distance);
-  if(distance<=5){
+  //set off alarm if distance is lte alarm value
+  if(distance<=range_alarm_value){
     speakjet.print(range_sounds);
   }
 }
@@ -206,7 +248,11 @@ void find_range(){
  * MEMORY FUNCTION(S)
  ***************************/
 /**
- *
+ * write a value to the current (saved) memory
+ * address.
+ * if the final available memory location is reached, 
+ * then the memory is "full" so we 
+ * change the control value to read.
  */
 void mem_write(){
   //record data from sensors starting at address 0
@@ -217,18 +263,25 @@ void mem_write(){
   }
 }
 /**
- *
+ * Read all data from memeory. 
+ * Once the data has been read in, reset all
+ * memeory locations using reset_mem().
  */
 void mem_read(){
-  if(Serial){
+  //wait until a Serial connection is made
+  if(Serial){ 
     readData();
     reset_mem();
   }
 }
 /**
- *
+ * loop through all available EEPROM 
+ * and output the value at each address 
+ * location.
  */
 void readData(){
+  //loop until the highest recorded bit and 
+  //output the data
   for(int i = 0; i<ADDRESSBIT; i++){
     float val = EEPROM.read(i);
     Serial.println(val, DEC);
@@ -237,18 +290,26 @@ void readData(){
   delay(100);
 }
 /**
- *
+ * Fully reset the EEPROM memory and 
+ * memory pointer values
  */
 void reset_mem(){
-  clearData();
+  //set all values to 0
+  clearData(); 
+  //set the address bit (saved address) back to zero
   EEPROM.write(ADDRESSBIT, 0);
+  //set controller to write
   EEPROM.write(CONTROLBIT, WRITE);
+  //reset address value to zero
   address_val = 0;
+  //output that the memory has been reset
   Serial.println("Memory Reset");
+  //wait for the serial connection to be closed
   while(Serial);
 }
 /**
- *
+ * Set all data in EEPROM address
+ * values to 0.00
  */
 void clearData(){
   for(int i = 0; i<ADDRESSBIT; i++){
@@ -261,7 +322,8 @@ void clearData(){
  * TIMING FUNCTION(S)
  ***************************/
 /**
- *
+ * timer that keeps relative time for
+ * the various sensors
  */
 void increment_timer(){
   timer=timer + 1;
@@ -274,7 +336,10 @@ void increment_timer(){
  * VOICEBOX FUNCTION(S)
  ***************************/
 /**
- *
+ * Sets voicebox shield to all 
+ * necessary initial values
+ * 
+ * used in Setup();
  */
 void setup_voicebox(){
   //Configure the pins for the SpeakJet module
